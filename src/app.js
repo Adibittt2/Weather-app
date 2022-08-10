a66f432dbb1c4cc11e55000511960bbdf8ecb800
Aditya Verma <adityaverma00796@gmail.com>
2022-08-10 18:35:09 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,51 @@
+
+const express = require("express");
+
+const path = require("path");
+const hbs = require("hbs");
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+
+// console.log(path.join(__dirname,"../public"));
+
+const static_path = path.join(__dirname,"../public");
+const views_path = path.join(__dirname,"../templates/views");
+const partials_path = path.join(__dirname,"../templates/partials");
+
+
+app.set('view engine','hbs');
+app.set("views",views_path);
+
+hbs.registerPartials(partials_path);
+
+app.use(express.static(static_path));
+
+// routing
+
+app.get("/",(req,res)=>{
+
+    res.render("index");
+});
+
+app.get("/about",(req,res)=>{
+    res.render("about");
+})
+
+app.get("/weather",(req,res)=>{
+    res.render("weather");
+
+})
+
+
+app.get("*",(req,res)=>{
+    res.render("error");
+})
+
+
+app.listen(port,()=>{
+
+    console.log(`Data send ${port}`);
+});
